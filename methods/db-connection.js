6eb908b2c5131d20f8fff63778a8831366e05979
Fégan PIERRE <fegan.pierre@gmail.com>
2018-10-26 09:55:03 +0200

    Fix wrong credential

--- methods/db-connection.js
+++ methods/db-connection.js
@@ -10,7 +10,7 @@ const db_connection = mysql.createConnection({
   host: 'us-cdbr-iron-east-01.cleardb.net',
   database: 'heroku_43c8e1c3b41665f',
   user: 'bacf05b4c11ff7',
-  password: 'bacf05b4c11ff7'
+  password: '50dc6ad9'
 })
 
 module.exports = db_connection
